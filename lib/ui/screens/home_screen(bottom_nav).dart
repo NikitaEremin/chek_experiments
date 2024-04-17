b5596fcc3a9ce5f1dd50af5fa96f2ceb8b50c0d8
Nikita <nik.ere2012@yandex.by>
2024-04-17 21:41:33 +0300

    icon change to payment

--- lib/ui/screens/home_screen(bottom_nav).dart
+++ lib/ui/screens/home_screen(bottom_nav).dart
@@ -34,8 +34,8 @@ class _HomeScreenState extends State<HomeScreen> {
         destinations: [
           NavigationDestination(
             icon: Icon(_selectedPageIndex == 0
-                ? Icons.point_of_sale
-                : Icons.point_of_sale_outlined),
+                ? Icons.payments
+                : Icons.payments_outlined),
             label: 'Касса',
           ),
           NavigationDestination(
